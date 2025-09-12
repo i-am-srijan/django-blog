28c6851725a5cdd86ca74e2368101ad0eaf4933e
Srijan Shrestha <sthas425@gmail.com>
2025-09-12 12:23:58 +0545

    comment feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect,get_object_or_404
-from django.http import HttpResponse
-from blogs.models import Blog,Category
+from django.http import HttpResponse, HttpResponseRedirect
+from blogs.models import Blog,Category, Comment
 from django.db.models import Q #complex lookups such as or operator
 
 
@@ -27,8 +27,22 @@ def post_by_category(request, category_id):
 
 def blogs(request, slug):
     single_blog = get_object_or_404(Blog, slug=slug, status ='Published')
+
+    if request.method == "POST":
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    # comments
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
+    # print('Comments=>', comments)
     context={
         'single_blog':single_blog,
+        'comments': comments,
+        'comment_count': comment_count,
     }
     return render(request,'blogs.html', context)
 
